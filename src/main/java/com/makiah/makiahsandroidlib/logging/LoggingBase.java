37e07e6fbf71572779020624f910293a023fb23b
Makiah Bennett <makiahtbennett@gmail.com>
2017-11-24 21:47:34 -0500

    Improve the way that logging works by adding a LoggingBase

---
+++ src/main/java/com/makiah/makiahsandroidlib/logging/LoggingBase.java
@@ -0,0 +1,87 @@
+package com.makiah.makiahsandroidlib.logging;
+
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import com.makiah.makiahsandroidlib.threading.ParallelTask;
+import com.makiah.makiahsandroidlib.threading.TaskParent;
+
+import java.util.ArrayList;
+
+public abstract class LoggingBase extends ParallelTask
+{
+    // Properties required for logging.
+    private static final int MAX_SEQUENTIAL_LINES = 13;
+    protected ArrayList<String> sequentialConsoleData; //Lines being added and removed.
+    protected ArrayList<ProcessConsole> privateProcessConsoles;
+
+    public LoggingBase(TaskParent parent)
+    {
+        super(parent, "Logging Task");
+
+        //Initialize required components.
+        sequentialConsoleData = new ArrayList<>();
+        privateProcessConsoles = new ArrayList<>();
+    }
+
+    /**
+     * Log a series of lines to the console.
+     * @param newLines the lines to log.
+     */
+    public void lines(String... newLines)
+    {
+        //Add new line at beginning of the lines.
+        for (String line: newLines)
+            sequentialConsoleData.add (0, line);
+        //If there is more than 5 lines there, remove one.
+        while (sequentialConsoleData.size () > MAX_SEQUENTIAL_LINES)
+            sequentialConsoleData.remove (MAX_SEQUENTIAL_LINES);
+    }
+
+    /**
+     * To get a private process console, create a new Log.ProcessConsole(<name here>) and then run write() to provide new content.
+     */
+    public ProcessConsole newProcessConsole(String name)
+    {
+        return new ProcessConsole(name, privateProcessConsoles);
+    }
+
+    /**
+     * Returns the entire console as a single concatenated string.
+     */
+    protected String getConsoleAsString()
+    {
+        String entireConsole = "";
+        entireConsole = entireConsole.concat("—————— Sequential Data —————————\n");
+        entireConsole = entireConsole.concat(TextUtils.join("\n", sequentialConsoleData));
+
+        if (privateProcessConsoles.size() > 0)
+        {
+            entireConsole = entireConsole.concat("\n—————— Process Console Data —————————\n");
+            for (ProcessConsole processConsole : privateProcessConsoles)
+            {
+                entireConsole = entireConsole.concat(TextUtils.join("\n", processConsole.processData));
+            }
+        }
+
+        return entireConsole;
+    }
+
+    /**
+     * Override: tells the console what to do for on-screen logging.
+     */
+    protected abstract void refreshOnScreenConsole();
+
+    /**
+     * The task which updates the console at a fairly slow rate but your eye can't tell the difference.
+     */
+    @Override
+    protected void onDoTask () throws InterruptedException
+    {
+        while (true)
+        {
+            refreshOnScreenConsole();
+            flow.msPause(300);
+        }
+    }
+}
--- src/main/java/com/makiah/makiahsandroidlib/logging/OnScreenLog.java
+++ src/main/java/com/makiah/makiahsandroidlib/logging/OnScreenLog.java
@@ -15,121 +15,33 @@ import java.util.ArrayList;
  *
  * Local so that resetting is not an issue.
  */
-public abstract class OnScreenLog
+public abstract class OnScreenLog extends LoggingBase
 {
     private final OnScreenLogParent parent;
     private final TextView log;
 
-    private static final int MAX_SEQUENTIAL_LINES = 13;
-    private ArrayList<String> sequentialConsoleData; //Lines being added and removed.
-    private ArrayList<ProcessConsole> privateProcessConsoles;
-
-    private ConsoleUpdater consoleUpdaterInstance = null;
-
     public OnScreenLog(OnScreenLogParent parent, TextView log)
     {
+        super(parent);
+
+        // Required to log to a text view item.
         this.parent = parent;
         this.log = log;
 
-        //Initialize required components.
-        sequentialConsoleData = new ArrayList<>();
-        privateProcessConsoles = new ArrayList<>();
-
-        startConsoleUpdater();
-    }
-
-    public void lines(String... newLines)
-    {
-        //Add new line at beginning of the lines.
-        for (String line: newLines)
-            sequentialConsoleData.add (0, line);
-        //If there is more than 5 lines there, remove one.
-        while (sequentialConsoleData.size () > MAX_SEQUENTIAL_LINES)
-            sequentialConsoleData.remove (MAX_SEQUENTIAL_LINES);
-    }
-
-    public void appendToLastSequentialLine (String toAppend)
-    {
-        String result = sequentialConsoleData.get (0) + toAppend;
-        sequentialConsoleData.remove (0);
-        sequentialConsoleData.add (0, result);
-    }
-
-    /**
-     * To get a private process console, create a new Log.ProcessConsole(<name here>) and then run write() to provide new content.
-     */
-
-    public ProcessConsole newProcessConsole(String name)
-    {
-        return new ProcessConsole(name, privateProcessConsoles);
-    }
-
-    /**
-     * The task which updates the console at a fairly slow rate but your eye can't tell the difference.
-     */
-    private class ConsoleUpdater extends ParallelTask
-    {
-        public ConsoleUpdater() {
-            super(parent, "Console Updater");
-        }
-
-        @Override
-        protected void onDoTask () throws InterruptedException
-        {
-            while (true)
-            {
-                refreshConsole();
-                flow.msPause(300);
-            }
-        }
-    }
-
-    /**
-     * Creates a new console updater instance and runs it.
-     */
-    public void startConsoleUpdater ()
-    {
-        if (consoleUpdaterInstance == null)
-        {
-            consoleUpdaterInstance = new ConsoleUpdater();
-            consoleUpdaterInstance.run();
-        }
-    }
-
-    /**
-     * Nullifies a new console instance and stops it.
-     */
-    public void stopConsoleUpdater ()
-    {
-        if (consoleUpdaterInstance != null)
-        {
-            consoleUpdaterInstance.stop ();
-            consoleUpdaterInstance = null;
-        }
+        this.run();
     }
 
     /**
      * Rebuilds the whole console (call minimally, allow the task to take care of it.)
      */
-    public void refreshConsole ()
+    public void refreshOnScreenConsole ()
     {
+        final String consoleContents = getConsoleAsString();
+
         parent.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                String entireConsole = "";
-                entireConsole = entireConsole.concat("—————— Sequential Data —————————\n");
-                entireConsole = entireConsole.concat(TextUtils.join("\n", sequentialConsoleData));
-
-                if (privateProcessConsoles.size() > 0)
-                {
-                    entireConsole = entireConsole.concat("\n—————— Process Console Data —————————\n");
-                    for (ProcessConsole processConsole : privateProcessConsoles)
-                    {
-                        entireConsole = entireConsole.concat(TextUtils.join("\n", processConsole.processData));
-                    }
-                }
-
-                log.setText(entireConsole);
+                log.setText(consoleContents);
             }
         });
     }
